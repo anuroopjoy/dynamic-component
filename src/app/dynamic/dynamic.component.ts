b91ae0ec084490606ebf880c7f5f5c70898b2ac0
Anuroop Joy <anuroop.joy@hrblock.com>
2021-07-18 00:52:44 +0530

    initialfinal

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -11,17 +11,18 @@ import { SharedService } from '../shared.service';
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
-  styleUrls: ['./dynamic.component.scss']
+  styleUrls: ['./dynamic.component.scss'],
 })
 export class DynamicComponent implements OnInit, OnChanges {
   @Input() public description: string = '';
   @Output() public clicked!: EventEmitter<void>;
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService) {
+    this.description = this.sharedService.description;
+    this.clicked = this.sharedService.clicked;
+  }
 
   ngOnInit(): void {
     console.log('inside dynamic init');
-    this.description = this.sharedService.description;
-    this.clicked = this.sharedService.clicked;
   }
   ngOnChanges(): void {
     console.log('inside dynamic changes');
